b4ba8689db3879053c34a68939fa935827096c89
HYIUYOU <821219285@qq.com>
2023-12-06 21:33:49 +0800

    first commit

---
+++ ch1/go.mod
@@ -0,0 +1,4 @@
+module github.com/HYIUYOU/my_go_learn_code
+//module helloworld
+
+go 1.21.5
---
+++ ch1/helloword/main.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界!")
+	//Go 语言原生支持 Unicode，它可以处理全世界任何语言的文本。
+}
